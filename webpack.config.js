0785fbd4456c800c0cf544eaf9616c46540d57c6
kai.wei <kai.wei@qunar.com>
2017-02-17 09:03:27 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var config = {
 
     //entry :{ "scripts/libs/common" : "./src/scripts/libs/common.js"},
-    entry : {"scripts/index" : "./src/scripts/index.js"},
+    entry : {"scripts/index" : "./src/scripts/index.js" , "style/index" : './src/styles/index.scss'},
 
     output : {
         path : __dirname + "/dist/",
-        publicPath : "/",
+        publicPath : "/asset",
         filename : "[name].js" 
     },
     externals : {
@@ -17,12 +17,19 @@ var config = {
         "vue" : "Vue"
     },
 
+    module : {
+        loaders: [
+            { test: /\.scss$/, loader: "sass-loader" }
+        ]
+    
+    }
+
     plugins : [
         new HtmlWebpackPlugin({
             inject : "body",
             template : "src/index.html",
             filename : "index.html",
-            chunks : ["scripts/libs/common", "scripts/index"]
+            chunks : ["scripts/index"]
         }) 
     ]
 };
